fcef8e6cd88f9eb9f30baf69c5915c5f02cabae8
Bart de Boer <bart.de.boer@divverence.com>
2020-01-03 17:15:23 +0100

    Split the logic in discrete steps

--- MazeSolver.cs
+++ MazeSolver.cs
@@ -19,66 +19,46 @@ namespace Maze
         {
             var response = await _client.EnterMaze(_maze);
             var options = response.PossibleActionsAndCurrentScore;
+            TrackExits(options, _exitCrumbs);
+            TrackCollectionPoints(options, _collectCrumbs);
 
+            options = await CollectAllPoints(options);
+            options = await CollectScoreInHand(options);
+            await GoToExit(options);
+        }
+
+        private async Task GoToExit(PossibleActionsAndCurrentScore options)
+        {
             while (true)
             {
-                //await Task.Delay(10);
-                TrackExits(options, _exitCrumbs);
-                if (options.CurrentScoreInHand == 0 && options.CurrentScoreInBag >= _maze.PotentialReward)
+                // Looking for exit!
+                if (options.CanExitMazeHere)
                 {
-                    // Looking for exit!
-                    if (options.CanExitMazeHere)
-                    {
-                        //Console.WriteLine("Enter please");
-                        //Console.ReadLine();
-                        await _client.ExitMaze();
-                        return;
-                    }
+                    //Console.WriteLine("Enter please");
+                    //Console.ReadLine();
+                    await _client.ExitMaze();
+                    return;
+                }
 
-                    // Looking for collection point!
-                    var stack = _exitCrumbs.OrderBy(st => st.Count).FirstOrDefault();
-                    if (stack == null)
-                    {
-                        continue;
-                    }
-
+                // Looking for known route to exit point
+                var stack = _exitCrumbs.OrderBy(st => st.Count).FirstOrDefault();
+                if (stack != null)
+                {
                     var dir = stack.Peek();
                     var step = ReverseDir(dir);
                     options = await MakeMove(step, _exitCrumbs, _collectCrumbs, _crawlCrumbs);
                     continue;
                 }
-
-                TrackCollectionPoints(options, _collectCrumbs);
-                if (options.CurrentScoreInHand + options.CurrentScoreInBag >= _maze.PotentialReward)
-                {
-                    if (options.CanCollectScoreHere)
-                    {
-                        options = (await _client.CollectScore())
-                            .PossibleActionsAndCurrentScore;
-                        continue;
-                    }
-
-                    // Looking for collection point!
-                    var stack = FindBestCollectStack(_collectCrumbs, _exitCrumbs);
-                    if (stack == null)
-                    {
-                        continue;
-                    }
-
-                    var dir = stack.Peek();
-                    var step = ReverseDir(dir);
-                    options = await MakeMove(step, _exitCrumbs, _collectCrumbs, _crawlCrumbs);
-                    continue;
-                }
-
-                // Collecting
-                var mostUsefulDirection = MostUsefulDir(options, _crawlCrumbs);
+                
+                // No known EP. Find the most useful direction to hopefully find one 
+                var mostUsefulDirection = MostUsefulDirForLocatingExitPoint(options, _crawlCrumbs);
                 if (mostUsefulDirection != null)
                 {
                     options = await MakeMove(mostUsefulDirection.Direction, _exitCrumbs, _collectCrumbs, _crawlCrumbs);
                     continue;
                 }
 
+                // Back-track if no new directions were found
                 if (_crawlCrumbs.Any())
                 {
                     var dir = ReverseDir(_crawlCrumbs.Peek());
@@ -86,8 +66,74 @@ namespace Maze
                     continue;
                 }
 
-                Console.WriteLine("Stuck!");
+                Console.WriteLine("No possible route to Exit Point!");
             }
+        }
+
+        private async Task<PossibleActionsAndCurrentScore> CollectScoreInHand(PossibleActionsAndCurrentScore options)
+        {
+            while (options.CurrentScoreInHand != 0)
+            {
+                if (options.CanCollectScoreHere)
+                {
+                    options = (await _client.CollectScore()).PossibleActionsAndCurrentScore;
+                    continue;
+                }
+
+                // Looking for known route to collection point
+                var stack = FindBestCollectStack(_collectCrumbs, _exitCrumbs);
+                if (stack != null)
+                {
+                    var dir = stack.Peek();
+                    var step = ReverseDir(dir);
+                    options = await MakeMove(step, _exitCrumbs, _collectCrumbs, _crawlCrumbs);
+                    continue;
+                }
+                
+                // No known CP. Find the most useful direction to hopefully find one 
+                var mostUsefulDirection = MostUsefulDirForLocatingCollectionPoint(options, _crawlCrumbs);
+                if (mostUsefulDirection != null)
+                {
+                    options = await MakeMove(mostUsefulDirection.Direction, _exitCrumbs, _collectCrumbs, _crawlCrumbs);
+                    continue;
+                }
+
+                // Back-track if no new directions were found
+                if (_crawlCrumbs.Any())
+                {
+                    var dir = ReverseDir(_crawlCrumbs.Peek());
+                    options = await MakeMove(dir, _exitCrumbs, _collectCrumbs, _crawlCrumbs);
+                    continue;
+                }
+
+                Console.WriteLine("No possible route to Collection Point!");
+            }
+
+            return options;
+        }
+
+        private async Task<PossibleActionsAndCurrentScore> CollectAllPoints(PossibleActionsAndCurrentScore options)
+        {
+            while (options.CurrentScoreInHand + options.CurrentScoreInBag < _maze.PotentialReward)
+            {
+                var mostUsefulDirection = MostUsefulDirForCollecting(options, _crawlCrumbs);
+                if (mostUsefulDirection != null)
+                {
+                    options = await MakeMove(mostUsefulDirection.Direction, _exitCrumbs, _collectCrumbs, _crawlCrumbs);
+                    continue;
+                }
+                
+                if (_crawlCrumbs.Any())
+                {
+                    var dir = ReverseDir(_crawlCrumbs.Peek());
+                    options = await MakeMove(dir, _exitCrumbs, _collectCrumbs, _crawlCrumbs);
+                    continue;
+                }
+
+                Console.WriteLine("Stuck while collecting!");
+            }
+
+            return options;
         }
 
         private static Stack<MoveDirection> FindBestCollectStack(List<Stack<MoveDirection>> collectCrumbs,
@@ -125,11 +171,12 @@ namespace Maze
         {
             try
             {
-                var retVal = (await _client.Move(direction))
+                var newOptions = (await _client.Move(direction))
                     .PossibleActionsAndCurrentScore;
-                if (retVal == null)
+                if (newOptions == null)
                     throw new ArgumentException();
-
+                
+                // Record the move in all crumbs
                 foreach (var st in exitCrumbs)
                     Push(st, direction);
 
@@ -138,7 +185,11 @@ namespace Maze
 
                 Push(crumbs, direction);
 
-                return retVal;
+                // Check for nearby exits and collectionPoints
+                TrackExits(newOptions, _exitCrumbs);
+                TrackCollectionPoints(newOptions, _collectCrumbs);
+
+                return newOptions;
             }
             catch (Exception e)
             {
@@ -153,21 +204,42 @@ namespace Maze
         private readonly List<Stack<MoveDirection>> _collectCrumbs = new List<Stack<MoveDirection>>();
         private readonly List<Stack<MoveDirection>> _exitCrumbs = new List<Stack<MoveDirection>>();
 
-        private static MoveAction MostUsefulDir(PossibleActionsAndCurrentScore options,
+        private static MoveAction MostUsefulDirForCollecting(PossibleActionsAndCurrentScore options,
             Stack<MoveDirection> crawlCrumbs)
         {
             var mostUsefulDir = options
                 .MoveActions
-                .Where(ma =>
-                    !ma.HasBeenVisited) // Don't ever go where we've been before (backtracking will get us there if needed)
+                .Where(ma => !ma.HasBeenVisited) // Don't ever go where we've been before (backtracking will get us there if needed)
                 .OrderBy(_ => 1)
                 .ThenBy(ma => ma.AllowsScoreCollection) // Un-prefer ScoreCollection Points, we'll get there later
                 .ThenBy(ma => ma.AllowsExit) // Un-prefer exits, we'll get there later
+                .ThenBy(ma => ma.Reward == 0) // Prefer reward directions over non-reward. It might be the last straw!
                 .ThenBy(ma => HugTheLeftWall(ma.Direction, crawlCrumbs))
                 //.ThenBy(ma => RandomGenerator.Next())
-                .ThenByDescending(ma =>
-                    ma.Direction) // Prefer Left over Down over Right over Up... no real reason, just for predictability
-                .ThenByDescending(ma => ma.Reward) // Prefer high reward directions. Gut feeling, no real reason...
+                .ThenByDescending(ma => ma.Direction) // Prefer Starting Left over Down over Right over Up... no real reason, just for predictability
+                .FirstOrDefault();
+            return mostUsefulDir;
+        }
+
+        private static MoveAction MostUsefulDirForLocatingCollectionPoint(PossibleActionsAndCurrentScore options,
+            Stack<MoveDirection> crawlCrumbs)
+        {
+            var mostUsefulDir = options
+                .MoveActions
+                .Where(ma => !ma.HasBeenVisited) // Don't ever go where we've been before (backtracking will get us there if needed)
+                .OrderBy(ma => ma.AllowsExit) // Un-prefer exits, we'll get there later
+                .ThenBy(ma => HugTheLeftWall(ma.Direction, crawlCrumbs))
+                .FirstOrDefault();
+            return mostUsefulDir;
+        }
+
+        private static MoveAction MostUsefulDirForLocatingExitPoint(PossibleActionsAndCurrentScore options,
+            Stack<MoveDirection> crawlCrumbs)
+        {
+            var mostUsefulDir = options
+                .MoveActions
+                .Where(ma => !ma.HasBeenVisited) // Don't ever go where we've been before (backtracking will get us there if needed)
+                .OrderBy(ma => HugTheLeftWall(ma.Direction, crawlCrumbs))
                 .FirstOrDefault();
             return mostUsefulDir;
         }
