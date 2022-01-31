322024751424453841bf10357bde70c80b2dd5ac
Bart de Boer <bart.de.boer@divverence.com>
2022-01-31 22:38:43 +0100

    Update to 1.0.1 of client package

--- CountingClient.cs
+++ CountingClient.cs
@@ -21,11 +21,6 @@ namespace Maze
             return await _amazeingClientImplementation.AllMazes();
         }
 
-        public Task<ICollection<MazeInfo>> AllMazes(CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
         private void IncreaseInvocationCount() => Invocations++;
 
         public async Task<PossibleActionsAndCurrentScore> CollectScore()
@@ -34,20 +29,10 @@ namespace Maze
             return await _amazeingClientImplementation.CollectScore();
         }
 
-        public Task<PossibleActionsAndCurrentScore> CollectScore(CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public async Task<PossibleActionsAndCurrentScore> EnterMaze(string mazeName)
         {
             IncreaseInvocationCount();
             return await _amazeingClientImplementation.EnterMaze(mazeName);
-        }
-
-        public Task<PossibleActionsAndCurrentScore> EnterMaze(string mazeName, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
         }
 
         public async Task ExitMaze()
@@ -56,20 +41,10 @@ namespace Maze
             await _amazeingClientImplementation.ExitMaze();
         }
 
-        public Task ExitMaze(CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public async Task ForgetPlayer()
         {
             IncreaseInvocationCount();
             await _amazeingClientImplementation.ForgetPlayer();
-        }
-
-        public Task ForgetPlayer(CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
         }
 
         public async Task<PlayerInfo> GetPlayerInfo()
@@ -78,20 +53,10 @@ namespace Maze
             return await _amazeingClientImplementation.GetPlayerInfo();
         }
 
-        public Task<PlayerInfo> GetPlayerInfo(CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public async Task<PossibleActionsAndCurrentScore> Move(Direction direction)
         {
             IncreaseInvocationCount();
             return await _amazeingClientImplementation.Move(direction);
-        }
-
-        public Task<PossibleActionsAndCurrentScore> Move(Direction direction, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
         }
 
         public async Task<PossibleActionsAndCurrentScore> PossibleActions()
@@ -100,32 +65,10 @@ namespace Maze
             return await _amazeingClientImplementation.PossibleActions();
         }
 
-        public Task<PossibleActionsAndCurrentScore> PossibleActions(CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public async Task RegisterPlayer(string name)
         {
             IncreaseInvocationCount();
             await _amazeingClientImplementation.RegisterPlayer(name);
         }
-
-        public Task RegisterPlayer(string name, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public string BaseUrl
-        {
-            get => _amazeingClientImplementation.BaseUrl;
-            set => _amazeingClientImplementation.BaseUrl = value;
-        }
-
-        public bool ReadResponseAsString
-        {
-            get => _amazeingClientImplementation.ReadResponseAsString;
-            set => _amazeingClientImplementation.ReadResponseAsString = value;
-        }
-    }
+   }
 }
