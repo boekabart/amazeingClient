623a66d9aa4be6bf3cffcdfcbbaa71889d586f81
Bart de Boer <bart.de.boer@divverence.com>
2022-01-31 22:06:51 +0100

    Switch to using the public nuget package

--- CountingClient.cs
+++ CountingClient.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using HightechICT.Amazeing.Client.Rest;
 
 namespace Maze
 {
@@ -19,6 +21,11 @@ namespace Maze
             return await _amazeingClientImplementation.AllMazes();
         }
 
+        public Task<ICollection<MazeInfo>> AllMazes(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
         private void IncreaseInvocationCount() => Invocations++;
 
         public async Task<PossibleActionsAndCurrentScore> CollectScore()
@@ -27,10 +34,20 @@ namespace Maze
             return await _amazeingClientImplementation.CollectScore();
         }
 
+        public Task<PossibleActionsAndCurrentScore> CollectScore(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public async Task<PossibleActionsAndCurrentScore> EnterMaze(string mazeName)
         {
             IncreaseInvocationCount();
             return await _amazeingClientImplementation.EnterMaze(mazeName);
+        }
+
+        public Task<PossibleActionsAndCurrentScore> EnterMaze(string mazeName, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
         }
 
         public async Task ExitMaze()
@@ -39,10 +56,20 @@ namespace Maze
             await _amazeingClientImplementation.ExitMaze();
         }
 
+        public Task ExitMaze(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public async Task ForgetPlayer()
         {
             IncreaseInvocationCount();
             await _amazeingClientImplementation.ForgetPlayer();
+        }
+
+        public Task ForgetPlayer(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
         }
 
         public async Task<PlayerInfo> GetPlayerInfo()
@@ -51,10 +78,20 @@ namespace Maze
             return await _amazeingClientImplementation.GetPlayerInfo();
         }
 
+        public Task<PlayerInfo> GetPlayerInfo(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public async Task<PossibleActionsAndCurrentScore> Move(Direction direction)
         {
             IncreaseInvocationCount();
             return await _amazeingClientImplementation.Move(direction);
+        }
+
+        public Task<PossibleActionsAndCurrentScore> Move(Direction direction, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
         }
 
         public async Task<PossibleActionsAndCurrentScore> PossibleActions()
@@ -63,10 +100,32 @@ namespace Maze
             return await _amazeingClientImplementation.PossibleActions();
         }
 
+        public Task<PossibleActionsAndCurrentScore> PossibleActions(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public async Task RegisterPlayer(string name)
         {
             IncreaseInvocationCount();
             await _amazeingClientImplementation.RegisterPlayer(name);
+        }
+
+        public Task RegisterPlayer(string name, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string BaseUrl
+        {
+            get => _amazeingClientImplementation.BaseUrl;
+            set => _amazeingClientImplementation.BaseUrl = value;
+        }
+
+        public bool ReadResponseAsString
+        {
+            get => _amazeingClientImplementation.ReadResponseAsString;
+            set => _amazeingClientImplementation.ReadResponseAsString = value;
         }
     }
 }
--- XyGrid.cs
+++ XyGrid.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Runtime.InteropServices;
+using HightechICT.Amazeing.Client.Rest;
 
 namespace Maze
 {
